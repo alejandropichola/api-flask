781805ff18a19a002a21b80064043658ae97c9d9
Alejandro <g4alejandro29@gmail.com>
2020-10-21 15:58:53 -0600

    config migrate

--- app/user/model/user.py
+++ app/user/model/user.py
@@ -7,9 +7,9 @@ class User(db.Model):
   phone = db.Column(db.String(20))
   email = db.Column(db.String(100))
   address = db.Column(db.String(300))
-  date = db.Column(db.DateTime),
-  temperature = db.Column(db.Numeric)
-  heartRate = db.Column('heart_rate', db.Numeric)
+  date = db.Column(db.DateTime)
+  temperature = db.Column(db.Numeric(10,2))
+  heartRate = db.Column('heart_rate', db.Numeric(10,2))
   hasMask = db.Column('has_mask', db.Boolean)
   hasFaceShield = db.Column('has_face_shield', db.Boolean)
   hasRules = db.Column('has_rules', db.Boolean)
@@ -17,3 +17,12 @@ class User(db.Model):
   def __init__(self, dpi, fullName):
     self.dpi = dpi
     self.fullName = fullName
+
+class UserSymptoms(db.Model):
+  __tablename__='user_symptoms'
+  id = db.Column(db.Integer, primary_key=True)
+  dpi = db.Column(db.Integer, db.ForeignKey('user.dpi'))
+  date = db.Column(db.DateTime)
+  symptoms = db.Column(db.Text)
+  temperature =db.Column(db.Numeric(10,2))
+  heartRate = db.Column('heart_rate', db.Numeric(10,2))
