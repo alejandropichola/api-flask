d83f8ed28dae6b8aedec802f0e6942b8ce1394ff
Alejandro <g4alejandro29@gmail.com>
2020-10-21 19:08:55 -0600

    config api

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, abort, jsonify
+from flask import Blueprint, request, abort
 from app import app
 from app.user.model.user import User
+import simplejson as json
 
 userController = Blueprint('userController', __name__)
 
@@ -8,8 +9,25 @@ userController = Blueprint('userController', __name__)
 def transactionUsers():
   dataRequest = request.get_json()
   response = dataRequest['data']
-  return jsonify('response')
+  return json.dumps('response')
+
 
 @app.route('/user', methods=['GET'])
 def getAllUser():
-  return jsonify('response')
+  data = User.query.all()
+  response = []
+  for item in data:
+    response.append({
+      'dpi': item.dpi,
+      'fullName': item.fullName,
+      'phone': item.phone,
+      'email': item.email,
+      'address': item.address,
+      'date': str(item.date),
+      'temperature': item.temperature,
+      'heartRate': item.heartRate,
+      'hasMask': item.hasMask,
+      'hasFaceShield': item.hasFaceShield,
+      'hasRules': item.hasRules
+    })
+  return json.dumps(response, use_decimal=True)
--- app/user/model/user.py
+++ app/user/model/user.py
@@ -2,8 +2,8 @@ from app import db
 
 class User(db.Model):
   __tablename__ = 'user'
-  dpi = db.Column(db.Integer, primary_key=True, autoincrement=False)
-  fullName = db.Column('full_name',db.String(100))
+  dpi = db.Column(db.String(100), primary_key=True)
+  name = db.Column('full_name',db.String(100))
   phone = db.Column(db.String(20))
   email = db.Column(db.String(100))
   address = db.Column(db.String(300))
@@ -14,15 +14,31 @@ class User(db.Model):
   hasFaceShield = db.Column('has_face_shield', db.Boolean)
   hasRules = db.Column('has_rules', db.Boolean)
 
-  def __init__(self, dpi, fullName):
+  def __init__(self, dpi, fullName, phone, email, address, date, temperature, heartRate, hasMask, hasFaceShield, hasRules):
     self.dpi = dpi
     self.fullName = fullName
+    self.phone = phone
+    self.email = email
+    self.address = address
+    self.date = date
+    self.temperature = temperature
+    self.heartRate = heartRate
+    self.hasMask = hasMask
+    self.hasFaceShield = hasFaceShield
+    self.hasRules = hasRules
 
 class UserSymptoms(db.Model):
   __tablename__='user_symptoms'
   id = db.Column(db.Integer, primary_key=True)
-  dpi = db.Column(db.Integer, db.ForeignKey('user.dpi'))
+  dpi = db.Column(db.String(100), db.ForeignKey('user.dpi'))
   date = db.Column(db.DateTime)
   symptoms = db.Column(db.Text)
   temperature =db.Column(db.Numeric(10,2))
   heartRate = db.Column('heart_rate', db.Numeric(10,2))
+
+  def __init__(self, dpi, date, symptoms, temperature, heartRate):
+    self.dpi = dpi
+    self.date = date
+    self.symptoms = symptoms
+    self.temperature = temperature
+    self.heartRate = heartRate
